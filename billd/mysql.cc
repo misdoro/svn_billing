0edd2e9134ae84ad1820e6d2776eb09cf268595f
misdoro <misdoro@797f3f01-adb0-dc11-a484-00e04c00235c>
2008-02-18 23:37:25 +0000

    Stats are now saved in DB, money are charged, but it seems that some locking issue exists somewhere near netflow or mysql listeners

--- billd/mysql.cc
+++ billd/mysql.cc
@@ -13,42 +13,50 @@ MYSQL * connectdb () {
 }
 
 void * statsupdater(void *threadid) {
+	printf("started stats offloading thread\n");
 	// update values in database
 	while (cfg.terminate == 0) {
-		pthread_mutex_lock (&users_table_m);
 		if ((time(NULL) - cfg.stats_updated_time) > cfg.stats_update_interval) {
 			printf("Updating stats in MySql...\n");        
 			cfg.stats_updated_time = time(NULL);
+			pthread_mutex_lock (&users_table_m);
 			for (user * u = firstuser; u != NULL; u = u->next) {
-				for (zone_group * p = u->first_zone_group; p != NULL; p = p->next) {
-					if ((p->group_changed == 1) || ((time(NULL) - u->die_time) > cfg.die_time_interval)) {
+				zone_group * p = u->first_zone_group;
+				pthread_mutex_unlock (&users_table_m);
+				pthread_mutex_lock (&(u->user_mutex));
+				double charged_money=0;
+				//Save stat record for each of user's groups:
+				while (p != NULL){
+					if (p->group_changed == 1) {
 						char * sql = new char[1024];
-						uint64_t in_bytes = p->in_bytes;
-						uint64_t out_bytes = p->out_bytes;
-						uint32_t pid = p->id;
-						uint32_t sid = u->session_id;
-						//float traf_in_money = p->in_mb_cost_total;
-						// connected=2 - user disconnected, but not removed from DB
-						//sprintf(sql, "UPDATE session_statistics SET traf_in=%llu, traf_out=%llu, traf_in_money=%f WHERE (session_id=%lu) AND (zone_group_id=%lu)", in_bytes, out_bytes, traf_in_money, sid, pid);
-						//mysql_query(cfg.myconn, sql);
-						//printf("%s\n",sql);
-						//delete sql;
+						double money = (double) p->in_diff * p->zone_mb_cost / (double) MB_LENGTH;
+						charged_money += money;
+						sprintf(sql, "insert into session_statistics (zone_group_id,session_id,traf_in,traf_out,traf_in_money) values (%lu,%lu,%llu,%llu,%f);",p->id,u->session_id,p->in_diff,p->out_diff,money);
+						mysql_query(cfg.myconn, sql);
+						printf("%s\n",sql);
+						delete sql;
+						p->in_diff=0;
+						p->out_diff=0;
 						p->group_changed = 0;
 					}
+					p = p->next;
 				}
-				if ((u->debit_changed == 1) || ((time(NULL) - u->die_time) > cfg.die_time_interval)) {
-					u->debit_changed = 0;
+				//Update user's debit:
+				if ( charged_money>0 ) {
 					char * sql = new char[1024];
-					sprintf(sql, "UPDATE users SET debit=debit-%f WHERE id=%u", u->user_debit_diff,u->id); /*Its better to update debit this way IMHO*/
+					sprintf(sql, "UPDATE users SET debit=debit-%f WHERE id=%u", charged_money,u->id); /*Its better to update debit this way IMHO*/
 					mysql_query(cfg.myconn, sql);
-					u->user_debit_diff=0;
 					printf("%s\n",sql);
-					delete sql;                
+					delete sql;
 				}
+				pthread_mutex_unlock (&(u->user_mutex));
+				pthread_mutex_lock (&users_table_m);
 			}
+			pthread_mutex_unlock (&users_table_m);
 		}
 		// for remove disconnected users from table
 		user * next_u;
+		pthread_mutex_lock (&users_table_m);
 		for (user * u = firstuser; u != NULL;) {
 			if (u->die_time == 0) continue;
 			if ((time(NULL) - u->die_time) > cfg.die_time_interval) {
