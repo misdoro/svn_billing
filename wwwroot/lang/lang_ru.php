c1f64e48e52ccc3809bf4229c4247ab99dde9b26
Mikhail Doronin <misdoro@users.noreply.github.com>
2008-11-28 01:12:56 +0000

    R64: Complete draft implementation of graphs.php Need to use FROM_UNIXTIME instead of UNIX_TIMESTAMP to speed up some queries.

--- wwwroot/lang/lang_ru.php
+++ wwwroot/lang/lang_ru.php
@@ -19,5 +19,8 @@ $lang['groupby']='Группировка:';
 $lang['group']['0']='Сводная статистика';
 $lang['group']['1']='Статистика по дням';
 $lang['group']['2']='Статистика по сессиям';
-
+$lang['traf_units']=array('','K','M','G');
+$lang['time_seconds']=array(1,60,3600,86400,2592000,31536000);
+$lang['time_units']=array('s','m','h','d','M','Y');
+$lang['time_units_long']=array('seconds','minutes','hours','days','months','years');
 ?>
