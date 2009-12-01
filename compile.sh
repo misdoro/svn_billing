ebfbcca80684c69f6f2bd94faf8378521f11c912
misdoro <misdoro@797f3f01-adb0-dc11-a484-00e04c00235c>
2009-12-01 23:11:12 +0000

    Fixed compilation script for FreeBSD

--- compile.sh
+++ compile.sh
@@ -1,1 +1,3 @@
-g++ -g -ggdb3 -pthread -ansi -lmysqlclient_r -L/usr/local/lib/mysql -I/usr/local/include -O2 -Wall -o bin/billd billd/*.cc billd/*.cpp
+#g++ -g -ggdb3 -pthread -ansi -lmysqlclient_r -L/usr/local/lib/mysql -I/usr/local/include -O2 -Wall -o bin/billd billd/*.cc billd/*.cpp
+g++ -pthread -lmysqlclient_r -L/usr/local/lib/mysql -I/usr/local/include -O2 -Wall -o bin/billd billd/*.cc billd/*.cpp
+
