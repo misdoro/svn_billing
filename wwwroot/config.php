fb41b09d3fa5e41628d1789f3536f7aa106aed0b
Mikhail Doronin <misdoro@users.noreply.github.com>
2009-12-23 18:46:58 +0000

    Implemented change price page Updated price_names table to support different price types

--- wwwroot/config.php
+++ wwwroot/config.php
@@ -1,9 +1,9 @@
 <?
-$mysql_host='127.0.0.1';
-//$mysql_host='192.168.64.252';
+//$mysql_host='127.0.0.1';
+$mysql_host='192.168.64.254';
 $mysql_port=3306;
-$mysql_user='billing';
-$mysql_password='billadm';
+$mysql_user='billadm';
+$mysql_password='billpasswd';
 $mysql_database='billing';
 $dbcharset='utf8';
 
@@ -22,7 +22,7 @@ $cash_admins=array('misdoro','flexx');
 
 
 //Business mode makes it look like all price units are in MB etc
-$business_mode=1;
+$business_mode=0;
 
 //Show traffic packs
 $show_packs=false;
